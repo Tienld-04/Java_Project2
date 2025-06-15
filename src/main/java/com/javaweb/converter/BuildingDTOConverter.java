c6b902cae8f45b9ef1f1b0862aac7872a59b6435
Le Danh Tien <danhtien2k4@gmail.com>
2025-06-15 14:40:18 +0700

    using modelmapper

--- src/main/java/com/javaweb/converter/BuildingDTOConverter.java
+++ src/main/java/com/javaweb/converter/BuildingDTOConverter.java
@@ -1,8 +1,9 @@
 package com.javaweb.converter;
-
+//Chuyen doi du lieu
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,23 +22,16 @@ public class BuildingDTOConverter {
 	@Autowired
 	private RentAreaRepository rentAreaRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public BuildingDTO toBuildingDTO(BuildingEntity item) {
-		BuildingDTO building = new BuildingDTO();
+		BuildingDTO building = modelMapper.map(item, BuildingDTO.class);
 		DistrictEntity districtEntity = districtRepository.findNameById(item.getDistrictid());
-		
-		building.setName(item.getName());
-		building.setNumberOfBasement(item.getNumberOfBasement());
 		building.setAddress(item.getStreet()+","+item.getWard()+","+districtEntity.getName() );
 		List<RentAreaEntity> rentAreas = rentAreaRepository.getValueByBuildingId(item.getId());
 		String listRentArea = rentAreas.stream().map(it -> it.getValue().toString()).collect(Collectors.joining(","));
 		building.setRentArea(listRentArea);
-		building.setManagerName(item.getManagerName());
-		building.setManagerPhoneNumber(item.getManagerPhoneNumber());
-		building.setFloorArea(item.getFloorArea());
-		building.setEmptyArea(item.getEmptyArea());
-		building.setRentPrice(item.getRentPrice());
-		building.setServiceFee(item.getServiceFee());
-		building.setBrokerageFee(item.getBrokerageFee());
 		return building;
 	}
 }
